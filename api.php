9402a55764519556ebf1e0d8b0b4118323a187fd
LETANG Alexandre <letang.alexandre88@gmail.com>
2018-11-09 16:41:59 +0100

    comments added

--- api.php
+++ api.php
@@ -1,6 +1,8 @@
 <?php
+//Include SQL link
 require "config.php";
 
+//Get http method
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'),true);
 
@@ -8,13 +10,15 @@ $input = json_decode(file_get_contents('php://input'),true);
 switch ($method) {
   case 'GET':
 
-   if((isset($_GET['count']))) {
+
+   if((isset($_GET['count']))) { //Custom count
      $count = $_GET['count'];
    }
    else {
      $count = 25;
    }
 
+   // paginate the list of data
    $page = 1;
    $start = 0;
    if(isset($_GET['page'])&&$_GET['page'] !="") {
@@ -26,12 +30,12 @@ switch ($method) {
    }
 
 
-   if(isset($_GET['author'])) {
+   if(isset($_GET['author'])) { //Search a specific author
      $author = $_GET['author'];
 
     $request = "SELECT * FROM tweet WHERE author = '$author' LIMIT $start, $count";
    }
-   elseif(isset($_GET['hashtags'])) {
+   elseif(isset($_GET['hashtags'])) { //Search a specific hashtags
      $hashtags = $_GET['hashtags'];
 
     $request = "SELECT * FROM tweet WHERE hashtags = '#' '$hashtags' LIMIT $start, $count";
@@ -39,7 +43,7 @@ switch ($method) {
    else{
     $request = "SELECT * FROM tweet LIMIT $start, $count" ;
    }
-
+   //sending the sql resquest and print data
     $exec = $pdo->query($request);
     $tab  =  array();
     while ($result = $exec->fetch(PDO::FETCH_OBJ)) {
@@ -50,11 +54,11 @@ switch ($method) {
   break;
 
   case 'POST':
-    if((isset($_POST["author"]))&&(isset($_POST["message"]))) {
+    if((isset($_POST["author"]))&&(isset($_POST["message"]))) {// check if a author and a message was send
       $author = $_POST["author"];
       $message = $_POST["message"];
 
-      if(isset($_POST["hashtags"])) {
+      if(isset($_POST["hashtags"])) {// if hahstags
         $hashtags = $_POST["hashtags"];
         $values = ['author' => $author, "message" => $message, "hashtags" => $hashtags];
         $request = "INSERT INTO tweet (author, message, hashtags) VALUES (:author, :message, :hashtags)";
@@ -63,8 +67,13 @@ switch ($method) {
         $values = ['author' => $author, "message" => $message];
         $request = "INSERT INTO tweet (author, message) VALUES (:author, :message)";
       }
+
+      //sending the resquest
     $send = $pdo->prepare($request);
     $send->execute($values);
+    }
+    else {
+
     }
 
   break;
